2f8c486fb04b017e4055e19dcad2565a63ca86dd
mooyg <mooybot@gmail.com>
2023-09-10 21:36:16 +0530

    chore

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,13 +4,19 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local on_attach = function(client, bufnr)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+				-- other maps
+			end
+
 			require("lspconfig").tsserver.setup({
 				capabilities = capabilities,
+				on_attach = on_attach,
 			})
 			require("lspconfig").tailwindcss.setup({
 				capabilities = capabilities,
 			})
-			require("lspconfig").svelteserver.setup({
+			require("lspconfig").svelte.setup({
 				capabilities = capabilities,
 			})
 			require("lspconfig").jsonls.setup({
@@ -58,7 +64,7 @@ return {
 					"vim",
 					"vimdoc",
 					"yaml",
-					"svelte"
+					"svelte",
 				},
 				highlight = {
 					enable = true,
@@ -129,6 +135,10 @@ return {
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-n>"] = cmp.mapping.select_next_item(),
 				}),
+				 window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
 			})
 		end,
 	},
@@ -151,7 +161,7 @@ return {
 					},
 					json = {
 						require("formatter.filetypes.json").prettierd,
-					}
+					},
 				},
 			})
 		end,
@@ -160,6 +170,64 @@ return {
 		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 		config = function()
 			require("lsp_lines").setup()
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>1", desc = "Harpoon buf 1" },
+			{ "<leader>2", desc = "Harpoon buf 2" },
+			{ "<leader>3", desc = "Harpoon buf 3" },
+			{ "<leader>4", desc = "Harpoon buf 4" },
+			{ "<leader>5", desc = "Harpoon buf 5" },
+			{ "<leader>6", desc = "Harpoon buf 6" },
+			{ "<leader>7", desc = "Harpoon buf 7" },
+			{ "<leader>8", desc = "Harpoon buf 8" },
+			{ "<leader>9", desc = "Harpoon buf 9" },
+			{ "<leader>0", desc = "Harpoon buf 10" },
+			{ "<leader>a", desc = "Harpoon add" },
+			{ "<leader>h", desc = "Harpoon menu" },
+		},
+		config = function()
+			require("harpoon")
+			local mark = require("harpoon.mark")
+			local ui = require("harpoon.ui")
+
+			vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon a file" })
+			vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "Harpoon menu" })
+			vim.keymap.set("n", "<leader>1", function()
+				ui.nav_file(1)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>2", function()
+				ui.nav_file(2)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>3", function()
+				ui.nav_file(3)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>4", function()
+				ui.nav_file(4)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>5", function()
+				ui.nav_file(5)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>6", function()
+				ui.nav_file(6)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>7", function()
+				ui.nav_file(7)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>8", function()
+				ui.nav_file(8)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>9", function()
+				ui.nav_file(9)
+			end, { silent = true })
+			vim.keymap.set("n", "<leader>0", function()
+				ui.nav_file(10)
+			end, { silent = true })
 		end,
 	},
 }
