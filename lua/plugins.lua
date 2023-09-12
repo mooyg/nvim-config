1226bcef9afef434c0e71cab2fdd6f54257be716
mooyg <mooybot@gmail.com>
2023-09-12 20:50:30 +0530

    chore

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,6 +22,24 @@ return {
 			require("lspconfig").jsonls.setup({
 				capabilities = capabilities,
 			})
+			vim.diagnostic.config({
+				virtual_text = false,
+				virtual_lines = true,
+			})
+			vim.api.nvim_create_autocmd("CursorHold", {
+				buffer = bufnr,
+				callback = function()
+					local opts = {
+						focusable = true,
+						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+						border = "rounded",
+						source = "always",
+						prefix = " ",
+						scope = "cursor",
+					}
+					vim.diagnostic.open_float(nil, opts)
+				end,
+			})
 		end,
 	},
 	{
